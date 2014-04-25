fd85db51bd6bff440aa7dd93b281dceb02a2c745
Oleksandr Poltavets <sancho.ck@gmail.com>
2014-04-25 19:08:42 +0300

    reviewing rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ describe "Static pages" do
   shared_examples_for "all static pages" do
     it { should have_selector('h1',   text: heading) }
     it { should have_selector('title', text: full_title(page_title)) }
-  end  
+  end
 
   describe "Home page" do
     before { visit root_path }
@@ -43,7 +43,7 @@ describe "Static pages" do
         it { should have_link("1 followers", href: followers_user_path(user)) }
       end
 
-    end    
+    end
   end
 
   describe "Help page" do
